825bb62525b328013637deee86ba6d343e71ed3a
Elsacha <elsacha8@gmail.com>
2016-03-25 15:26:04 -0400

    Added Angularjs routing

--- client/js/app.js
+++ client/js/app.js
@@ -1,16 +1,63 @@
-angular
-  .module('app', [
-    'lbServices',
-    'ui.router'
-  ])
-  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
-      $urlRouterProvider) {
-    $stateProvider
-      .state('engrus', {
-        url: '',
+//var app = angular
+//  .module('app', [
+//    'lbServices',
+//    'ui.router'
+//  ])
+//  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
+//      $urlRouterProvider) {
+//    $stateProvider
+//      .state('engrus_search', {
+//        url: '',
+//        templateUrl: 'views/engrus_search.html',
+//        controller: 'EngRusController'
+//      })
+//      .state('result', {
+//        url: 'engrus_result',
+//        templateUrl: 'views/engrus_result.html',
+//        controller: 'EngRusController'
+//      });
+//    
+//
+//    $urlRouterProvider.otherwise('');
+//  }]);
+
+var app = angular.module('app', ['lbServices', 'ngRoute']);
+
+app.config(function ($routeProvider) {
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'views/engrus_search.html',
+        controller: 'EngRusController'
+    })
+    
+    .when('/engrus_result', {
         templateUrl: 'views/engrus_result.html',
         controller: 'EngRusController'
-      });
+    })
+    
+});
 
-    $urlRouterProvider.otherwise('engrus');
-  }]);
+//var app = angular.module('app', ['lbServices', 'ui.router']);
+//
+//app.config(function($stateProvider, $urlRouterProvider) {
+//    
+//    $urlRouterProvider.otherwise('/');
+//    
+//    $stateProvider
+//        
+//        .state('search', {
+//            url: '/',
+//            templateUrl: 'views/engrus_search.html',
+//            controller: 'EngRusController'
+//        })
+//    
+//        
+//        .state('result', {
+//            url: 'engrus_result',
+//            templateUrl: 'views/engrus_result.html',
+//            controller: 'EngRusController'
+//        });
+//        
+//});
--- client/js/controllers/engrus.js
+++ client/js/controllers/engrus.js
@@ -1,61 +1,61 @@
 angular
   .module('app')
-  .controller('EngRusController', ['$scope', '$state', 'EngRus', 
-                function($scope, $state, EngRus) {
+  .controller('EngRusController', ['$scope', 'EngRus', '$http', 'sourceService',
+                function($scope, EngRus, $http, sourceService) {
     $scope.sourceTerm =  sourceService.srcTerm;
       
     $scope.$watch('srcTerm', function() {
        sourceService.srcTerm = $scope.srcTerm; 
     });
+//      
+//    function getEngRus() {
+//      EngRus
+//        .find()
+//        .$promise
+//        .then(function(results) {
+//          $scope.engrus = results;
+//        });
+//    }
+//    getEngRus();
       
-    function getEngRus() {
-      EngRus
-        .find()
-        .$promise
-        .then(function(results) {
-          $scope.engrus = results;
-        });
-    }
-    getEngRus();
-      
-//$http.
-//    get('/api/EngRus/?filter={"where": { "eng": "'+ $scope.srcTerm + '" } }'.
-//    success(function(data) {
-//      console.log(JSON.stringify(data));
-//      //$scope.target = JSON.stringify(data);
-//        $scope.target = data;
-//    });
+$http.
+    get('/api/EngRus/?filter={"where": { "eng": "'+ $scope.sourceTerm + '" } }').
+    success(function(data) {
+      console.log(JSON.stringify(data));
+      //$scope.target = JSON.stringify(data);
+        $scope.target = data;
+    });
 
-    $scope.addEngRus = function() {
-      EngRus
-        .create($scope.newEngRus)
-        .$promise
-        .then(function(enru) {
-          $scope.newEngRus = '';
-          $scope.engrusForm.content.$setPristine();
-          $('.focus').focus();
-          getEngRus();
-        });
-    };
-      
-    $scope.searchEngRus = function(term) {
-      EngRus
-        .find($scope.newEngRus)
-        .$promise
-        .then(function(enru) {
-          $scope.newEngRus = '';
-          $scope.engrusForm.content.$setPristine();
-          $('.focus').focus();
-          getEngRus();
-        });
-    };
-
-    $scope.removeEngRus = function(item) {
-      EngRus
-        .deleteById(item)
-        .$promise
-        .then(function() {
-          getEngRus();
-        });
-    };
+//    $scope.addEngRus = function() {
+//      EngRus
+//        .create($scope.newEngRus)
+//        .$promise
+//        .then(function(enru) {
+//          $scope.newEngRus = '';
+//          $scope.engrusForm.content.$setPristine();
+//          $('.focus').focus();
+//          getEngRus();
+//        });
+//    };
+//      
+//    $scope.searchEngRus = function(term) {
+//      EngRus
+//        .find($scope.newEngRus)
+//        .$promise
+//        .then(function(enru) {
+//          $scope.newEngRus = '';
+//          $scope.engrusForm.content.$setPristine();
+//          $('.focus').focus();
+//          getEngRus();
+//        });
+//    };
+//
+//    $scope.removeEngRus = function(item) {
+//      EngRus
+//        .deleteById(item)
+//        .$promise
+//        .then(function() {
+//          getEngRus();
+//        });
+//    };
   }]);
